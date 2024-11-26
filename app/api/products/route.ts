62de796a4d93f9b23456196b03fc1b7751970176
Victory Nwani <vickywane@gmail.com>
2024-11-26 01:51:44 +0000

    update: explicitly specify clientURL for medusa product service

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -45,6 +45,8 @@ export async function GET(request: NextRequest, res: NextResponse) {
       });
     }
 
+    console.log("process.env.POSTGRES_URL =>", process.env.POSTGRES_URL,);
+
     const productService = await initializeProductModule({
       database: {
         clientUrl: process.env.POSTGRES_URL,
